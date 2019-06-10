f9d7fa582f91e677ea01d53d75890b464605e5e1
Alejandro Beiderman <abeiderman@gmail.com>
2019-06-09 21:42:55 -0400

    Don't build activerecord 6 in travis ci.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 bundle exec appraisal
-bundle exec appraisal rake
+bundle exec appraisal activerecord-5 rake
+bundle exec appraisal faker rake
+bundle exec appraisal ffaker rake
+bundle exec rake
