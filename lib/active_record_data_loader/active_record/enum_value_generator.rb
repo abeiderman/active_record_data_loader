f0c616b5deb1d024e5647b14265c6c3e26cb1359
Alejandro Beiderman <abeiderman@gmail.com>
2019-06-23 13:22:32 -0400

    Fix rubocop warning

--- lib/active_record_data_loader/active_record/enum_value_generator.rb
+++ lib/active_record_data_loader/active_record/enum_value_generator.rb
@@ -30,7 +30,7 @@ module ActiveRecordDataLoader
             .compact
         end
 
-        def mysql_enum_values_for(model_class, enum_type)
+        def mysql_enum_values_for(_model_class, enum_type)
           enum_type
             .to_s
             .downcase
