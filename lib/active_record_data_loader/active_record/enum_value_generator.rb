f90cb29805a0e9104128cad7af4e132d496f4444
Alejandro Beiderman <abeiderman@gmail.com>
2021-11-06 21:03:11 -0700

    Fix enum generator to use the connection factory
    It was getting a connection from the model instead of the provided
    connection factory.

--- lib/active_record_data_loader/active_record/enum_value_generator.rb
+++ lib/active_record_data_loader/active_record/enum_value_generator.rb
@@ -5,34 +5,33 @@ module ActiveRecordDataLoader
     class EnumValueGenerator
       class << self
         def generator_for(model_class:, ar_column:, connection_factory:)
-          values = enum_values_for(model_class, ar_column.sql_type, connection_factory)
+          values = enum_values_for(ar_column.sql_type, connection_factory)
           -> { values.sample }
         end
 
         private
 
-        def enum_values_for(model_class, enum_type, connection_factory)
+        def enum_values_for(enum_type, connection_factory)
           connection = connection_factory.call
 
           if connection.adapter_name.downcase.to_sym == :postgresql
-            postgres_enum_values_for(model_class, enum_type)
+            postgres_enum_values_for(connection, enum_type)
           elsif connection.adapter_name.downcase.to_s.start_with?("mysql")
-            mysql_enum_values_for(model_class, enum_type)
+            mysql_enum_values_for(enum_type)
           else
             []
           end
         end
 
-        def postgres_enum_values_for(model_class, enum_type)
-          model_class
-            .connection
+        def postgres_enum_values_for(connection, enum_type)
+          connection
             .execute("SELECT unnest(enum_range(NULL::#{enum_type}))::text")
             .map(&:values)
             .flatten
             .compact
         end
 
-        def mysql_enum_values_for(_model_class, enum_type)
+        def mysql_enum_values_for(enum_type)
           enum_type
             .to_s
             .downcase
